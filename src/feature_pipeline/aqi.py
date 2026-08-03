d66ea3f1f90388af3b2f16c8285102a05270debc
ali591195 <ali.zahid3428@gmail.com>
2026-08-03 19:45:20 +0500

    feat: implement Open-Meteo air quality feature collection pipeline

---
+++ src/feature_pipeline/aqi.py
@@ -0,0 +1,86 @@
+from math import floor
+
+from src.feature_pipeline.schemas import Pollutants, IdealGasParams
+from src.feature_pipeline.constants import (POLLUTANTS_CONVERSION_DATA, UNIVERSAL_GAS_CONSTANT,
+                                            POLLUTANTS_MAX_CONCENTRATION, POLLUTANTS_BREAKPOINTS)
+
+
+def calculate_pollutants_aqis(pollutants: Pollutants, ideal_gas_law_params: IdealGasParams) -> Pollutants:
+    """
+        Convert each pollutant into its respective aqi
+
+        :param pollutants: Pollutant values
+        :param ideal_gas_law_params: Contain temperature and pressure
+        :return: A dictionary of pollutants aqi
+        :raises ValueError: When value is invalid
+    """
+
+    convert_to_epa_units(temp_c=ideal_gas_law_params["temp"], pressure_hpa= ideal_gas_law_params["pressure"], pollutants=pollutants)
+
+    # Formatting for EQA
+    format_pollutants(pollutants)
+
+    aqis: Pollutants = {}
+
+    for pollutant, concentration in pollutants.items():
+        if concentration < 0:
+            raise ValueError(f"{pollutant.capitalize()} cannot be negative.")
+
+        if concentration > POLLUTANTS_MAX_CONCENTRATION[pollutant]:
+            aqis[pollutant] = 500
+        else:
+
+            # Apply formula
+            for c_low, c_high, i_low, i_high in POLLUTANTS_BREAKPOINTS[pollutant]:
+                if c_low <= concentration <= c_high:
+                    aqi = (
+                            (i_high - i_low)
+                            / (c_high - c_low)
+                            * (concentration - c_low)
+                            + i_low
+                    )
+                    aqis[pollutant] = round(aqi)
+                    break
+
+            if pollutant not in aqis:
+                raise ValueError(f"Invalid {pollutant} concentration.")
+
+    return aqis
+
+
+def format_pollutants(pollutants: Pollutants) -> None:
+    """
+        Format pollutants according to EPA
+
+        :param pollutants: Pollutant values
+        :return: None
+    """
+
+    pollutants["pm10"] = floor(pollutants["pm10"])
+    pollutants["pm25"] = floor(pollutants["pm25"] * 10) / 10
+
+    pollutants["o3"] = floor(pollutants["o3"] * 1000) / 1000
+    pollutants["co"] = floor(pollutants["co"] * 10) / 10
+
+    pollutants["no2"] = floor(pollutants["no2"])
+    pollutants["so2"] = floor(pollutants["so2"])
+
+
+def convert_to_epa_units(temp_c: float, pressure_hpa: float, pollutants: Pollutants) -> None:
+    """
+        Convert pollutants to EPA units
+
+        :param temp_c: Temperature in Celsius
+        :param pressure_hpa: Pressure
+        :param pollutants: Pollutant values
+        :return: None
+    """
+
+    temp_k = temp_c + 273.15
+    pressure_pa = pressure_hpa * 100
+
+    for pollutant, (molecular_weight, unit) in POLLUTANTS_CONVERSION_DATA.items():
+        pollutants[pollutant] = (pollutants[pollutant] * UNIVERSAL_GAS_CONSTANT * temp_k) / (pressure_pa * molecular_weight)
+
+        if unit == "ppb":
+            pollutants[pollutant] *= 1000
---
+++ src/feature_pipeline/feature_pipeline.py
@@ -0,0 +1,82 @@
+# Imports
+import requests
+from requests import Response
+
+from src.feature_pipeline.parsers import parse_openmeteo_response
+from src.feature_pipeline.constants import OPENMETEO_AIRQUALITY_URL, OPENMETEO_WEATHER_URL, TIMEOUT
+
+
+def get_openmeteo_params(hourly: list[str]) -> dict[str, str | list[str] | float]:
+    """
+        Give params for open-meteo api request
+
+        :param hourly: Hourly params list
+        :return: Request params
+    """
+
+    return {
+	"latitude": 31.558,
+	"longitude": 74.3507,
+	"hourly": hourly,
+	"start_date": "2026-08-03",
+	"end_date": "2026-08-03",
+}
+
+
+# Endpoints parameters
+openmeteo_airquality_params = get_openmeteo_params(["pm10", "pm2_5", "carbon_monoxide", "nitrogen_dioxide", "sulphur_dioxide", "ozone"])
+openmeteo_weather_params = get_openmeteo_params(["temperature_2m", "dew_point_2m", "relative_humidity_2m", "surface_pressure", "wind_speed_10m"])
+
+
+def get_exception_detail(url: str, error: requests.exceptions.RequestException) -> dict[str, str]:
+    """
+        Return a formatted error object
+
+        :param url: The url endpoint
+        :param error: The raised exception
+        :return: A formatted error object
+    """
+
+    error_details = {
+        "url": url,
+        "type": error.__class__.__name__,
+        "message": str(error)
+    }
+
+    if error.response is not None:
+        error_details["response"] = error.response.text
+
+    return error_details
+
+
+def fetch_data(url: str, params: dict[str, str | list[str] | float]) -> Response | None:
+    """
+        Fetch data from the given url
+
+        :param url: The url endpoint
+        :param params: The url parameters
+        :return: A Response object or none if exception is raised
+    """
+    try:
+        resp = requests.get(url, params=params, timeout=TIMEOUT)
+
+        # Raise status errors
+        resp.raise_for_status()
+
+        return resp
+
+    except requests.exceptions.RequestException as e:
+        print(get_exception_detail(url, e))
+        return None
+
+
+# Fetch current pollutant data & current weather conditions for Lahore
+openmeteo_airquality_response = fetch_data(OPENMETEO_AIRQUALITY_URL, openmeteo_airquality_params)
+openmeteo_weather_response = fetch_data(OPENMETEO_WEATHER_URL, openmeteo_weather_params)
+
+
+
+
+if openmeteo_airquality_response is not None and openmeteo_weather_response is not None:
+        parse_openmeteo_response(openmeteo_airquality_response, openmeteo_weather_response)
+
