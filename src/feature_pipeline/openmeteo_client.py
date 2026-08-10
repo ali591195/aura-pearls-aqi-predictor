2839bd27ab17199b7004acb90f54b774b473af1b
ali591195 <ali.zahid3428@gmail.com>
2026-08-10 17:59:04 +0500

    refactor: extract reusable Open-Meteo data collection utilities

---
+++ src/feature_pipeline/openmeteo_client.py
@@ -0,0 +1,72 @@
+from typing import Literal
+
+import requests
+from requests import Response
+
+from src.feature_pipeline.constants import LAHORE_LATITUDE, LAHORE_LONGITUDE, TIMEOUT
+from src.feature_pipeline.schemas import DateRange
+
+
+def get_openmeteo_params(date_range: DateRange, mode: Literal["AIR_QUALITY", "WEATHER"]) -> dict[str, float | list[str] | str]:
+    """
+        Build request parameters for the Open-Meteo API.
+
+        :param date_range: Tuple containing the start and end dates (YYYY-MM-DD).
+        :param mode: Selects either the Air Quality or Weather endpoint parameters.
+        :return: Dictionary of request parameters for the Open-Meteo API.
+    """
+
+    # Hourly parameter per mode
+    air_quality_hour_params = ["pm10", "pm2_5", "carbon_monoxide", "nitrogen_dioxide", "sulphur_dioxide", "ozone"]
+    weather_hour_params = ["temperature_2m", "dew_point_2m", "relative_humidity_2m", "surface_pressure", "wind_speed_10m"]
+
+    # Dates extraction
+    start_date, end_date = date_range
+
+    return {
+        "latitude": LAHORE_LATITUDE,
+        "longitude": LAHORE_LONGITUDE,
+        "hourly": air_quality_hour_params if mode == "AIR_QUALITY" else weather_hour_params,
+        "start_date": start_date,
+        "end_date": end_date,
+    }
+
+def get_exception_detail(url: str, error: requests.exceptions.RequestException) -> dict[str, str]:
+    """
+        Return a formatted error object
+
+        :param url: The url endpoint
+        :param error: The raised exception
+        :return: A formatted error object
+    """
+
+    error_details = {
+        "url": url,
+        "type": error.__class__.__name__,
+        "message": str(error)
+    }
+
+    if error.response is not None:
+        error_details["response"] = error.response.text
+
+    return error_details
+
+def fetch_openmeteo_data(url: str, params: dict[str, str | list[str] | float]) -> Response | None:
+    """
+        Fetch data from the given url
+
+        :param url: The url endpoint
+        :param params: The url parameters
+        :return: A Response object or none if exception is raised
+    """
+    try:
+        resp = requests.get(url, params=params, timeout=TIMEOUT)
+
+        # Raise status errors
+        resp.raise_for_status()
+
+        return resp
+
+    except requests.exceptions.RequestException as e:
+        print(get_exception_detail(url, e))
+        return None
