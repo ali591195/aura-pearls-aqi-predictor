ef40554d6ab316f84feb87854a83bd92ca2d6509
ali591195 <ali.zahid3428@gmail.com>
2026-08-11 19:53:08 +0500

    feat(recovery): add daily and weekly recovery pipelines

---
+++ src/feature_pipeline/daily_recovery.py
@@ -0,0 +1,17 @@
+from datetime import datetime, UTC, timedelta
+
+from src.feature_pipeline.constants import OPENMETEO_WEATHER_URL
+from src.feature_pipeline.data_collector import collect_and_insert_features
+
+
+def run_daily_recovery() -> None:
+    """
+        Run the daily recovery pipeline
+
+        :return: None
+    """
+
+    yesterday = (datetime.now(UTC) - timedelta(days=1)).strftime("%Y-%m-%d")
+    date_range = (yesterday, yesterday)
+
+    collect_and_insert_features(OPENMETEO_WEATHER_URL, "BACKFILL", date_range)
