fd61a5de12b81d45314eb54f28886f527fd58970
ali591195 <ali.zahid3428@gmail.com>
2026-09-05 14:59:38 +0500

    feat: support mixed model types in prediction service

--- backend/app/services/model_service.py
+++ backend/app/services/model_service.py
@@ -2,11 +2,13 @@ import joblib
 import shutil
 from pathlib import Path
 
+import lightgbm as lgb
 from keras import Sequential
 from keras.src.saving import load_model
 from sklearn.preprocessing import RobustScaler
+from xgboost import XGBRegressor
 
-from src.common.constants import MODEL_DIR
+from src.common.constants import MODEL_DIR, CURRENT_PROD_MODELS
 from src.common.hopsworks_client import mr
 
 
@@ -66,13 +68,20 @@ def get_models() -> list[Sequential]:
 
     models = []
 
-    for day in range(1, 5):
-        model_name = f"aqi_mlp_day_{day}"
+    for model_tuple in CURRENT_PROD_MODELS:
+        model_name, model_type = model_tuple
         model_version = _get_latest_version(model_name)
 
         model_dir = MODEL_DIR / model_name
 
-        artifact_name = f"{model_name}.keras"
+        if model_type == "XGBoost":
+            artifact_name = f"{model_name}.json"
+        elif model_type == "MLP":
+            artifact_name = f"{model_name}.keras"
+        elif model_type == "LightGBM":
+            artifact_name = f"{model_name}.txt"
+        elif model_type == "Random Forest":
+            artifact_name = f"{model_name}.joblib"
 
         if not _is_version_downloaded(
                 model_dir,
@@ -87,8 +96,21 @@ def get_models() -> list[Sequential]:
             )
             _save_version(model_dir, model_version.version)
 
-        model = load_model(model_dir / artifact_name)
-        models.append(model)
+        model_path = model_dir / artifact_name
+
+        if model_type == "XGBoost":
+            model = XGBRegressor()
+            model.load_model(model_path)
+        elif model_type == "MLP":
+            model = load_model(model_path)
+        elif model_type == "LightGBM":
+            model = lgb.Booster(
+                model_file=model_path
+            )
+        elif model_type == "Random Forest":
+            model = joblib.load(model_path)
+
+        models.append((model, model_type))
 
     return models
 
